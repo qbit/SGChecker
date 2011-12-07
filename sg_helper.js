1704fd162d33a49fc8750980927e36969baace24
Aaron Bieber <qbit@deftly.net>
2011-12-07 14:59:08 -0700

    added helper script stuff

--- sg_helper.js
+++ sg_helper.js
@@ -1,15 +1,25 @@
-function addNameToImage() {
-	var sgImages  =  document.getElementsByTagName( 'img' );
-	for( var i = 0, l = sgImages.length; i < l; i++){
-			if( sgImages[i].src.indexOf("/girls/") > -1){
-				var parts = sgImages[i].src.split( '/' );
-				var name = parts[5];
-				var album = parts[7];
-				sgImages[i].title = name + ' / ' + unescape( album );
+var sg_helpers = [
+	{ 
+		name: "addNameToImage", 
+		desc: "Adds girl name / album name to the alt tag of an image for easy set identification from group posts, boards.",
+		fn: function() {
+			var sgImages  =  document.getElementsByTagName( 'img' );
+
+			for( var i = 0, l = sgImages.length; i < l; i++ ){
+				if( sgImages[i].src.indexOf("/girls/") > -1 ){
+					var parts = sgImages[i].src.split( '/' );
+					var name = parts[5];
+					var album = parts[7];
+					sgImages[i].title = name + ' / ' + unescape( album );
+				}
 			}
+		}
 	}
-}
+];
 
 (function(){
-	addNameToImage();
+	var i,l;
+	for ( i = 0, l = sg_helpers.length; i < l; i++ ) {
+		sg_helpers[i].fn();
+	}
 })()
